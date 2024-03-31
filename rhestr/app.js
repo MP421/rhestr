b9cc7c9e893f36ed8dec41c2640477401642a803
Jonathan Owen <jonathandowen1989@gmail.com>
2024-03-31 12:35:05 +0100

    Add files via upload

--- rhestr/app.js
+++ rhestr/app.js
@@ -5,6 +5,21 @@ const rhestrListItemEntryEl = document.querySelectorAll('.rhestr-list-item');
 const faqToggleBtns = document.querySelectorAll('.rhestr-q-and-a-item-toggle');
 const faqAnswers = document.querySelectorAll('.rhestr-q-and-a-item-answer');
 
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+];
+
 // Live demo functionality
 rhestrInputEl.addEventListener('keydown', (e) => {
   if (e.key === 'Enter' && rhestrInputEl.value !== '') {
@@ -26,6 +41,20 @@ rhestrInputEl.addEventListener('keydown', (e) => {
     deleteRhestrListItem.classList.add('rhestr-list-delete');
     deleteRhestrListItem.innerHTML = '<i class="fa-solid fa-trash"></i>';
     generateContainerEl.append(deleteRhestrListItem);
+
+    // Add timestamp to each task list item
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const timestamp = `${hours}:${minutes}`;
+    generateContainerEl.append(timestamp);
+
+    // Add date to each task list item
+    const date = now.getDate();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+    const currentDate = `${date} ${months[month]} ${year}`;
+    generateContainerEl.append(currentDate);
 
     // Toggle complete status indicator
     generateRhestrListItem.addEventListener('click', () => {
@@ -51,7 +80,7 @@ faqToggleBtns.forEach(faqToggleBtn => {
     const parentItem = faqToggleBtn.closest('.rhestr-q-and-a-item');
     if (parentItem) {
       const faqAnswerEl = parentItem.querySelector('.rhestr-q-and-a-item-answer');
-      if (parentItem.querySelector('.rhestr-q-and-a-item-answer')) {
+      if (faqAnswerEl) {
         faqAnswerEl.classList.toggle('active');
       };
     };
